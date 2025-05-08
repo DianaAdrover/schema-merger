f0f206a0d3fd3ca5e73809d7bd1a65303106910e
dadrover75 <dadrover75@gmail.com>
2025-05-08 06:35:17 -0300

    local styled and functional

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/schema-merger' : '',
+    images: {
+        unoptimized: true,
+    }
+}
 
-export default nextConfig;
+module.exports = nextConfig
