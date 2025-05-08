dc7e8cf5ac07e2f299cfe82b3f6e0df6eac16b04
dadrover75 <dadrover75@gmail.com>
2025-05-08 19:26:26 -0300

    lint fixes

--- src/app/api/schema/[id]/[filename]/route.ts
+++ src/app/api/schema/[id]/[filename]/route.ts
@@ -24,6 +24,6 @@ export async function GET(request: NextRequest) {
 
         return NextResponse.json({ content: JSON.stringify(schema, null, 2) });
     } catch (error) {
-        return NextResponse.json({ error: 'Server error' }, { status: 500 });
+        return NextResponse.json({ error: error }, { status: 500 });
     }
 }
